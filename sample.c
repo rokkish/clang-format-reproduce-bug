2dc7b9e24d3f980644c1a3a7383c0f695080b735
rokkish <thehiro117@gmail.com>
2023-11-07 21:09:37 +0900

    init

---
+++ sample.c
@@ -0,0 +1,8 @@
+
+typedef struct { /* include bitfield structure */
+    unsigned char x;        /* comment x */
+    unsigned int a  : 1;    /* comment a */
+    unsigned int b  : 1;    /* comment b */
+    unsigned int c  : 1;    /* comment c */
+    unsigned int aa : 1;    /* comment aa */
+} bitfield_t;
